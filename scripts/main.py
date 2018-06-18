ba3031e2eb2f2cecb8c197cf03031e2949a1ad97
sabSAThai <pranavsankhe40@gmail.com>
2018-06-18 10:54:13 +0900

    modified the code and added plots of spectrogram and MFCC

--- scripts/main.py
+++ scripts/main.py
@@ -4,9 +4,11 @@ import librosa
 
 filepath = '../test_audio/turkish_march.mp3' 
 y, sr = librosa.load(filepath)
-y = y[:200000]
+y = y[0:200000]
 
-utils.spectrogram(y, 32, sr, plotFlag=False)
-utils.mfcc(y,sr, 40, plotFlag=False)
+hopsize = 32
+utils.spectrogram(y, hopsize, sr, plotFlag=False)
+n_coeffs = 40
+utils.mfcc(y, sr, n_coeffs, plotFlag=True)
 
 
