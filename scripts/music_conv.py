fb78de08c692516d1434a566cd5701b5c3b43c81
sabSAThai <pranavsankhe40@gmail.com>
2018-06-22 16:43:39 +0900

    added mel spectrum functionality and pitch

--- scripts/music_conv.py
+++ scripts/music_conv.py
@@ -6,6 +6,7 @@ from scipy.io.wavfile import write
 from numpy import exp, abs, angle
 from scipy import signal
 import utils
+import librosa
 def audio_fft():
 
     rate, data = wav.read('31beethovens3a.wav')
@@ -14,19 +15,19 @@ def audio_fft():
     return [fft_out, signs]
 
 
-#mrate, data = wav.read('mond_1.wav')
-# #print data[0,:]
-# chunk = data[:,0]
-# kernel = [-2,0,-2]
-# conv1 = np.convolve(chunk, kernel)
+filepath = '../test_audio/poem.mp3' 
+data, sr = librosa.load(filepath)
+chunk = data
+kernel = [-1,-10, -1, -10, -1]
+conv1 = np.convolve(chunk, kernel)
 
 # chunk = data[:,1]
-# kernel = [-2,0,-2]
+# kernel = [1,0,1]
 # conv2 = np.convolve(chunk, kernel)
 # conv = np.hstack((conv1, conv2))
 # conv = conv.astype(np.float32)
-# conv = conv/10000
-# write('conv.wav', 44100, conv)
+conv1 = conv1/100
+write('conv.wav', sr, conv1)
 
 
 
